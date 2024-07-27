cc704839acd93a968605236d587ddd7cdcf8bbd1
chltjdfkr517@naver.com <chltjdfkr517@naver.com>
2024-07-27 23:12:43 +0900

    update

--- rak_front/src/components/Dashboard.jsx
+++ rak_front/src/components/Dashboard.jsx
@@ -1,20 +1,61 @@
 import React from "react";
 import './Dashboard.css';
-import { Chart } from 'react-google-charts';
 import FullCalendar from '@fullcalendar/react'
 import dayGridPlugin from '@fullcalendar/daygrid'
+import { Bar, Line, Doughnut, Pie } from 'react-chartjs-2';
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  PointElement,
+  LineElement,
+  ArcElement,
+  Title,
+  Tooltip,
+  Legend,
+} from 'chart.js';
 
+// Chart.js 모듈 등록
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  PointElement,
+  LineElement,
+  ArcElement,
+  Title,
+  Tooltip,
+  Legend
+);
 
 const Dashboard = () => {
 
-  const data = [["Age", "Weight"], [4, 5.5], [8, 12]];
-  const options = {
-    title: "Chart Title",
-    colors: ["#2980b9", "#3498db"],
-    titleTextStyle: {
-      fontSize: 24
-    }
+  const data = {
+    labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+    datasets: [
+      {
+        label: 'My First dataset',
+        backgroundColor: 'rgba(255,99,132,0.2)',
+        borderColor: 'rgba(255,99,132,1)',
+        borderWidth: 1,
+        hoverBackgroundColor: 'rgba(255,99,132,0.4)',
+        hoverBorderColor: 'rgba(255,99,132,1)',
+        data: [65, 59, 80, 81, 56, 55, 40]
+      }
+    ]
   };
+
+  const options = {
+    maintainAspectRatio: false,
+    responsive: true,
+    scales: {
+      y: {
+        beginAtZero: true
+      }
+    }
+
+  }
 
   const events = [
     { title: 'Event 1', date: '2023-07-01' },
@@ -26,13 +67,9 @@ const Dashboard = () => {
     <div className="Dashboard">
       <div className="row">
         <div className="col-md-6">
-          <div className="chart-container border p-3"><Chart
-            chartType="BarChart"
-            data={data}
-            options={options}
-            width="100%"
-            height="100%"
-          /></div>
+          <div className="chart-container border p-3">
+            <Bar data={data} options={options} width={100} height={400} />
+          </div>
         </div>
         <div className="col-md-6">
           <div className="calendar-container border p-3">
@@ -50,22 +87,14 @@ const Dashboard = () => {
           </div>
         </div>
         <div className="col-md-6">
-          <div className="chart-container border p-3"><Chart
-            chartType="AreaChart"
-            data={data}
-            options={options}
-            width="100%"
-            height="400px"
-          /></div>
+          <div className="chart-container border p-3">
+            <Line data={data} options={options} width={100} height={300} />
+          </div>
         </div>
         <div className="col-md-6">
-          <div className="chart-container border p-3"> <Chart
-            chartType="ColumnChart"
-            data={data}
-            options={options}
-            width="100%"
-            height="400px"
-          /></div>
+          <div className="chart-container border p-3">
+            <Doughnut data={data} options={options} width={100} height={300} />
+          </div>
         </div>
       </div>
     </div>
