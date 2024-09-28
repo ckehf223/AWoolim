19fcfcef2b4ee43c6dc098dca38b67e6e14a09d0
ckehf223 <ckehf223@gmail.com>
2024-09-28 19:20:11 +0900

    corsMvcConfig update

--- backend/Awoolim/src/main/java/com/kh/awoolim/common/config/CorsMvcConfig.java
+++ backend/Awoolim/src/main/java/com/kh/awoolim/common/config/CorsMvcConfig.java
@@ -8,20 +8,17 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class CorsMvcConfig implements WebMvcConfigurer {
 
-    @Override
-    public void addCorsMappings(CorsRegistry corsRegistry) {
-        corsRegistry.addMapping("/**")
-                    .allowedOrigins("*")  // 모든 도메인 허용
-                    .allowedMethods("*")
-                    .allowedHeaders("*")
-                    .exposedHeaders("Authorization", "LoginId")
-                    .allowCredentials(true)
-                    .maxAge(6000L);
-    }
+	@Override
+	public void addCorsMappings(CorsRegistry corsRegistry) {
+		corsRegistry.addMapping("/**")
+				.allowedOrigins("http://localhost:5173", "http://ec2-3-35-137-210.ap-northeast-2.compute.amazonaws.com")
+				.allowedMethods("*").allowedHeaders("*").exposedHeaders("Authorization", "LoginId")
+				.allowCredentials(true).maxAge(6000L);
+	}
 
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/swagger-ui/**")
-                .addResourceLocations("classpath:/META-INF/resources/webjars/swagger-ui/");
-    }
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/swagger-ui/**")
+				.addResourceLocations("classpath:/META-INF/resources/webjars/swagger-ui/");
+	}
 }
